4172545a7e41d1bd1bf9d2e15d78a08e21e8ab2b
Ayush Dadoo <ayush.dadoo@hitachids.com>
2026-03-16 14:50:31 +0530

    bbbb

--- factoryiq_src/factory_hourly_refresh.py
+++ factoryiq_src/factory_hourly_refresh.py
@@ -6,7 +6,7 @@ def main():
     ts = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     logging.info(f"Factory Hourly Refresh started at {ts}")
     print("Running factory hourly refresh tasks...")
-    print("hello ayush5")
+    print("hello ayush6")
     logging.info("Factory Hourly Refresh completed.")
 
 if __name__ == "__main__":
